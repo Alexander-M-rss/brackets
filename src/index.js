ed990d492ab837643436bc1f6f743cb6d9742ecb
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-02-01 20:55:28 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+    const closeBrackets = bracketsConfig.map((x) => x[1]);
+    const bracketsPair = new Map(bracketsConfig.map((x) => x.reverse()));
+    let stack = [];
+    
+    // console.log(closeBrackets);
+    // console.log(bracketsPair);
+        
+    for (let i = 0; i < str.length; i++) {
+    		// console.log(i, stack , str[i]);
+        if(stack.length === 0) {
+            if(closeBrackets.includes(str[i]))
+                if(bracketsPair.get(str[i]) === str[i])
+                    stack.push(str[i]);
+                else    
+                    return false;
+            else
+                stack.push(str[i]);
+        } else if (closeBrackets.includes(str[i])) {
+        		// console.log(bracketsPair.get(str[i]),stack[stack.length-1]);
+            if(bracketsPair.get(str[i]) === stack[stack.length-1])
+                stack.pop();
+            else
+                if(bracketsPair.get(str[i]) === str[i])
+                    stack.push(str[i]);
+                else    
+                    return false;           
+        } else 
+            stack.push(str[i]);
+ 			// console.log(stack);           
+        
+    }
+    // console.log(stack.length === 0);
+    return stack.length === 0;
 }
+
